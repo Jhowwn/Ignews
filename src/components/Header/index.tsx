3b2314e07d1f1f0ae4d43ec79f998287f111e439
LetsWorkb1 <jhow.win@gmail.com>
2022-03-21 09:38:16 -0300

    Criação da página posts e estilização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
+import { ActiveLink } from '../ActiveLink';
+
 import { SignInButton } from '../SingInButton';
+
 import styles from './styles.module.scss';
 
-export function Header () {
+export function Header() {
+    
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="logo do ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href='/'>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
